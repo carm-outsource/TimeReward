40b17ad9f286a6d0497e63c3655612347cbd829f
CarmJos <KarmunJ@outlook.com>
2022-02-28 19:48:18 +0800

    异步加载与卸载数据

--- src/main/java/cc/carm/plugin/timereward/listener/UserListener.java
+++ src/main/java/cc/carm/plugin/timereward/listener/UserListener.java
@@ -1,5 +1,6 @@
 package cc.carm.plugin.timereward.listener;
 
+import cc.carm.plugin.timereward.Main;
 import cc.carm.plugin.timereward.TimeRewardAPI;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,12 +11,12 @@ public class UserListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        TimeRewardAPI.getUserManager().load(event.getPlayer().getUniqueId());
+        Main.getInstance().getScheduler().runAsync(() -> TimeRewardAPI.getUserManager().load(event.getPlayer().getUniqueId()));
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-        TimeRewardAPI.getUserManager().unload(event.getPlayer().getUniqueId());
+        Main.getInstance().getScheduler().runAsync(() -> TimeRewardAPI.getUserManager().unload(event.getPlayer().getUniqueId()));
     }
 
 }
